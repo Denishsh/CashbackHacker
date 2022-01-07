d4842447bf842c94bc80499184dbd5b82529c61e
test <test>
2022-01-08 02:15:34 +0300

    test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,18 +1,37 @@
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import ru.netology.service.CashbackHackService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceTest {
     public CashbackHackService service;
+    public static CashbackHackService serviceNew;
+
+    // test
 
     @org.junit.Before
     public void init() {
         service = new CashbackHackService();
     }
 
+    @BeforeAll
+    static void initNew() {
+        serviceNew = new CashbackHackService();
+    }
+
+
     @org.junit.Test
     public void remainMoreTest() {
         Assert.assertEquals(999, service.remain(1001));
         Assert.assertEquals(995, service.remain(1005));
+    }
+
+    @Test
+    public void remainMoreNewTest() {
+        assertEquals(999, serviceNew.remain(1001));
+        assertEquals(995, serviceNew.remain(1005));
     }
 
     @org.junit.Test
@@ -22,9 +41,20 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(1, service.remain(999));
     }
 
+    @Test
+    public void remainLess1000NewTest() {
+        assertEquals(1000, serviceNew.remain(0));
+        assertEquals(999, serviceNew.remain(1));
+        assertEquals(1, serviceNew.remain(999));
+    }
+
     @org.junit.Test
     public void remain1000Test() {
         Assert.assertEquals(0, service.remain(1000));
     }
 
+    @Test
+    public void remain1000NewTest() {
+        assertEquals(0, serviceNew.remain(1000));
+    }
 }
